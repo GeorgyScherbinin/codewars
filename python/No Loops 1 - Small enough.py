5aaee5d9332a4276dd6f927b0010f50f453b8d06
Georgy Scherbinin <sherbg@mail.ru>
2025-08-11 16:03:42 +0700

    1

--- python/No Loops 1 - Small enough.py
+++ python/No Loops 1 - Small enough.py
@@ -1,3 +1,15 @@
 # https://www.codewars.com/kata/57cc4853fa9fc57a6a0002c2
-def 
-#
+
+def small_enough(a, limit,): 
+    if len(a) == 0: 
+        return True
+    else:
+        if a[0] <= limit:
+            a.pop(0)
+            return small_enough(a, limit)
+        else:
+            return False
+    
+
+
+print(small_enough([78, 110, 110, 99, 104, 117, 107, 115], 116))
