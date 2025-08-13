8cb90549c3020a4df110d14f13fa560893441fe2
Georgy Scherbinin <sherbg@mail.ru>
2025-08-13 08:39:10 +0700

    Changes to be committed: 	modified:   python/Stones on the Table.py  Untracked files: 	python/zip_lesson.py

--- python/Stones on the Table.py
+++ python/Stones on the Table.py
@@ -1,13 +1,17 @@
 # https://www.codewars.com/kata/5f70e4cce10f9e0001c8995a/train/python
 
+# def solution(stones):
+#     result = 0
+#     for i in range(len(stones) - 1):
+#         if stones[i] == stones[i + 1]: result += 1
+#     return result
+
 def solution(stones):
-    result = 0
-    for i in range(len(stones) - 1):
-        if stones[i] == stones[i + 1]: result += 1
-    return result
+    res = sum([a==b for a,b in zip(stones, stones[1:])])
+    print(list(zip(stones, stones[2:])))
+    
+    return  res
 
 
+print(solution("ABCD"))
 
-print(solution("RGGRGBBRGRR"))
-print(solution("RGBRGBRGGB"))
-print(solution("RRRRGGGGBBBB"))
