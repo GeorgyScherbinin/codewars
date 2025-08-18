8a60a8ed59e8a73751f76e1c2f68725d1b9d770d
Georgy Scherbinin <sherbg@mail.ru>
2025-08-18 11:05:45 +0700

    modified:   python/Simple Fun #237 Suffix Sums.py

--- python/Simple Fun #237 Suffix Sums.py
+++ python/Simple Fun #237 Suffix Sums.py
@@ -6,11 +6,19 @@
 # b[n - 2] =                 a[n - 2] + a[n - 1]
 # b[n - 1] =                            a[n - 1]
 
+# def suffix_sums(arr):
+#     arr_b = [0 for i in range(len(arr))]
+#     for i in range(len(arr)):
+#         for j in range(i, len(arr)):
+#             arr_b[i] += arr[j] 
+#     return arr_b
+
 def suffix_sums(arr):
-    arr_b = [0 for i in range(len(arr))]
-    for i in range(len(arr)):
-        for j in range(i, len(arr)):
-            arr_b[i] += arr[j] 
+    length = len(arr)
+    arr_b = [0 for i in range(length)]
+    arr_b[0] = sum(arr)
+    for i in range(1, length):
+        arr_b[i] = arr_b[i - 1] - arr[i -  1]
     return arr_b
 
 
