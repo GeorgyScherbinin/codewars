501cf7e46ff2572fea8640b35a1de931a8c56192
Georgy Scherbinin <sherbg@mail.ru>
2025-08-20 15:06:57 +0700

    modified:   hello world.py

--- python/hello world.py
+++ python/hello world.py
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-print('')
-print('hello world')
-print('')
-print('hello world')
-print('')
-print('hello world')
-print('')
-print('hello world')
-print('')
-print('hello world')
-=======
->>>>>>> 70349801afbd2ea2f511634c1e01fbc6e25f62fe
 
 print('hello world1')
 
