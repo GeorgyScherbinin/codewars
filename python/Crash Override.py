3dac6f9e0aa2e3a239831691ea24c92b0dec0753
Georgy Scherbinin <sherbg@mail.ru>
2025-08-20 14:58:08 +0700

    1

--- python/Crash Override.py
+++ python/Crash Override.py
@@ -1,6 +1,6 @@
 # https://www.codewars.com/kata/578c1e2edaa01a9a02000b7f/train/python
 
-#from preloaded import FIRST_NAME, SURNAME 123
+# from preloaded import FIRST_NAME, SURNAME 123
 
 def alias_gen(f_name: str, l_name: str) -> str:
     return  FIRST_NAME[f_name[0].capitalize()] + ' ' + SURNAME[l_name[0].capitalize()] if f_name[0].isalpha() and l_name[0].isalpha() else 'Your name must start with a letter from A - Z.'
