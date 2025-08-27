fa37ae8ee988c4e0905a24c9669e053674942671
Georgy Scherbinin <sherbg@mail.ru>
2025-08-27 10:51:05 +0700

    new file

--- python/Name to Matrix.py
+++ python/Name to Matrix.py
@@ -1,11 +1,22 @@
+from math import ceil
 # https://www.codewars.com/kata/5a91e0793e9156ccb0003f6e/train/python
 
+def matrixfy(st):  
+    k = 0
+    if st == '': return 'name must be at least one letter'
+    sq_len = ceil(len(st) ** 0.5)
+    print(sq_len)
+    res_list = [["." for j in range(sq_len)] for i in range(sq_len)]
 
-def matrixfy(st):  #12345
-    return None
-#1234 from testbranch
+    for i in range(sq_len):
+        for j in range(sq_len):
+            try:
+                res_list[i][j] = st[k]
+                k += 1
+            except:
+                return res_list
+    return res_list
 
-    # sffdsf
-    #Добавил коммент из тестовой ветки
-    # fff
+# matrixfy("Beyonce"), [["B", "e", "y"], ["o", "n", "c"], ["e", ".", "."]]
 
+print(matrixfy("Beonc"))
