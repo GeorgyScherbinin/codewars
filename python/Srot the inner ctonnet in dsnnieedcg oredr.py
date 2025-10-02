217e0735099cbdca79e395a48f90f37d0d7dcfb7
Georgy Scherbinin <sherbg@mail.ru>
2025-10-02 14:04:01 +0700

    1

---
+++ python/Srot the inner ctonnet in dsnnieedcg oredr.py
@@ -0,0 +1,22 @@
+# https://www.codewars.com/kata/5898b4b71d298e51b600014b/train/python
+def sort_the_inner_content(words):
+  list_of_words = words.split()
+  result_list = []
+
+  for word in list_of_words:
+    result_list.append(word[0])
+
+  return None
+
+# print(sort_the_inner_content("wait for me"))
+
+start_string = "wait for me"
+splitted_start_string = start_string.split()
+list_of_editted_start_string = []
+
+for word in splitted_start_string:
+  word 
+
+print(splitted_start_string)
+
+
