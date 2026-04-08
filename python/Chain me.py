99069bfcd6a21ba07fe25e2e4d826c5039630e11
Georgy Scherbinin <sherbg@mail.ru>
2026-04-08 14:55:02 +0700

    1

--- python/Chain me.py
+++ python/Chain me.py
@@ -1,15 +1,15 @@
 #https://www.codewars.com/kata/54fb853b2c8785dd5e000957
 
 def chain(init_val, functions):
-    result = 0
+    result = init_val
     for function in functions:
-        result += function(init_val)
+        result = function(init_val)
+        init_val = result
 
     return result
 
-def mul30(x): return x * 30
 def add10(x): return x + 10
-
+def mul30(x): return x * 30
 
 print(chain(50, [add10, mul30]))
 # returns 1800
